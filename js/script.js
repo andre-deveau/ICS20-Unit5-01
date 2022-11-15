1d130d17992119cc758263ecf2645ea93c305976
andre-deveau <andre-deveau@users.noreply.github.com>
2022-11-15 19:06:47 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
 /**
  * This function updates the slider value.
  */
- if (navigator.serviceWorker) {
+if (navigator.serviceWorker) {
   navigator.serviceWorker.register(
     "/ICS2O-Unit5-01-HTML-Slider-Example/sw.js",
     {
@@ -18,20 +18,20 @@
   )
 }
 
-const randomNumebr = Math.floor(Math.random() * 6) + 1;
+const randomNumebr = Math.floor(Math.random() * 6) + 1
 
 function updateSliderValue(valueFromSlider) {
   document.getElementById("answer").innerHTML = valueFromSlider
 
-    // process
-    if (valueFromSlider == randomNumber) {
-      document.getElementById("answer").innerHTML =
-        "The answer was, " + randomNumber + "!" + " You got it! Good Job."
-    }
+  // process
+  if (valueFromSlider == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was, " + randomNumber + "!" + " You got it! Good Job."
+  }
 
-    // blocks of code to be executed if condition1 is true
-    if (valueFromSlider != randomNumber) {
-      document.getElementById("answer").innerHTML =
-        "The answer was, " + randomNumber + "!" + " Nice guess, but try again."
-    }
+  // blocks of code to be executed if condition1 is true
+  if (valueFromSlider != randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was, " + randomNumber + "!" + " Nice guess, but try again."
+  }
 }
