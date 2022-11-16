b908d3372af690b8b7b802d3c53a424275b7e8a3
Andre <113055933+andre-deveau@users.noreply.github.com>
2022-11-16 14:08:02 -0500

    change script.js

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register(
-    "/ICS2O-Unit5-01-HTML-Slider-Example/sw.js",
+    "/ICS2O-Unit5-01/sw.js",
     {
-      scope: "/ICS2O-Unit5-01-HTML-Slider-Example/",
+      scope: "/ICS2O-Unit5-01/",
     }
   )
 }
