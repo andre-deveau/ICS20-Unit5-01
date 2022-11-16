3340d49a685f2b3ce4cec1051b69a01cc5d7994b
Andre <113055933+andre-deveau@users.noreply.github.com>
2022-11-16 14:19:35 -0500

    change script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Andre Deveau All rights reserved
 //
 // Created by: Andre Deveau
-// Created on: Oct 2022
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
